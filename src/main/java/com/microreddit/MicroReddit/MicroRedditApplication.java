9f625502fe5b8f7a3fdfe1ea53d1bdf2f01a2219
Jeff Morton <jmorton138@gmail.com>
2023-07-01 10:12:46 +0100

    first commit

---
+++ src/main/java/com/microreddit/MicroReddit/MicroRedditApplication.java
@@ -0,0 +1,13 @@
+package com.microreddit.MicroReddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroRedditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microreddit/MicroReddit/MicroRedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microreddit.MicroReddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroRedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
